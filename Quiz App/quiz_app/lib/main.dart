9d13dcae40f669dc030bf8b73471696dbabf3e1d
Syed Jafor <sjhnadeem@gmail.com>
2022-05-13 14:43:03 +0600

    8th commit

--- Quiz App/quiz_app/lib/main.dart
+++ Quiz App/quiz_app/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Text('Hello!'),
